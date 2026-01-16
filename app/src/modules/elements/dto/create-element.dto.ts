d3a7e5ceb51cd4e6d0cb563e9bd6ee69a1ae2ef0
Maksym <seokont@gmail.com>
2026-01-16 11:02:45 +0200

    add swagger and class-validation

--- app/src/modules/elements/dto/create-element.dto.ts
+++ app/src/modules/elements/dto/create-element.dto.ts
@@ -1,12 +1,63 @@
+import {
+  IsString,
+  IsInt,
+  MinLength,
+  Max,
+  Min,
+  IsEmail,
+  IsNotEmpty,
+  IsObject,
+} from 'class-validator';
+
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateElementDto {
+  @ApiProperty({ example: 'Описание', description: 'Тип элемента' })
+  @IsNotEmpty()
   type: string;
+
+  @ApiProperty({ example: 100, description: 'Позиция X элемента' })
+  @IsInt()
+  @Min(0)
   x: number;
+
+  @ApiProperty({ example: 150, description: 'Позиция Y элемента' })
+  @IsInt()
+  @Min(0)
   y: number;
+
+  @ApiProperty({ example: 200, description: 'Ширина элемента' })
+  @IsInt()
+  @Min(0)
   width: number;
+
+  @ApiProperty({ example: 100, description: 'Высота элемента' })
+  @IsInt()
+  @Min(0)
   height: number;
+
+  @ApiProperty({ example: 1, description: 'Прозрачность элемента' })
+  @IsInt()
+  @Min(0)
   opacity: number;
+
+  @ApiProperty({ example: 0, description: 'Масштаб элемента' })
+  @IsInt()
+  @Min(0)
   scale: number;
+
+  @ApiProperty({ example: {}, description: 'Свойства элемента' })
+  @IsObject()
   props: object;
+
+  @ApiProperty({ example: {}, description: 'Метаданные элемента' })
+  @IsObject()
   meta: object;
+
+  @ApiProperty({
+    example: 'ba5b1cf3-1f4a-41c3-a477-94d238bb72f5',
+    description: 'ID пользователя',
+  })
+  @IsString()
   userId: string;
 }
